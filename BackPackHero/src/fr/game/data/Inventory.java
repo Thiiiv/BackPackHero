33a4e011c8f9a9895a8c92575bfc72e12466957e
Thiiiv <105984503+Thiiiv@users.noreply.github.com>
2023-04-01 17:49:40 +0200

    Add files via upload

---
+++ BackPackHero/src/fr/game/data/Inventory.java
@@ -0,0 +1,44 @@
+package fr.game.data;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import fr.game.data.item.Item;
+
+public class Inventory {
+	private final Item[][] inventory;
+	
+	public Inventory() {
+		this.inventory = new Item[3][5];
+	}
+	public List<Item> showContains() {
+		List<Item> list = new ArrayList<>();
+		for (var ligne = 0; ligne < inventory.length; ligne++) {
+			for (var colonne = 0; colonne < inventory[0].length; colonne++) {
+				list.add(inventory[ligne][colonne]);
+			}
+		}
+		return list;
+	}
+	
+	public String add(int x, int y, Item item) {
+		if (x < inventory.length && y <= inventory[0].length) {
+			if (inventory[x][y] != null) {
+				inventory[x][y] = item;
+				return "L'item " + item + " a bien été ajouté à l'inventaire";
+			}
+		}
+		return "L'item n'a pas pu être ajouté";
+	}
+	
+	@Override
+	public String toString() {
+		List<Item> list = this.showContains();
+		StringBuilder sb = new StringBuilder();
+		sb.append("Contenu de l'inventaire :\n");
+		for (var i : list) {
+			sb.append(i + "\n");
+		}
+		return sb.toString();
+	}
+}
---
+++ BackPackHero/src/fr/game/data/Merchant.java
@@ -0,0 +1,21 @@
+package fr.game.data;
+
+import java.util.ArrayList;
+
+import fr.game.data.item.Item;
+
+public class Merchant implements Room {
+	@Override
+	public String getName() {
+		return "merchant";
+	}
+
+	@Override
+	public int display() {
+		return 0;
+	}
+	public ArrayList<Item> generateItems() {
+		return new ArrayList<Item>();
+	}
+
+}
