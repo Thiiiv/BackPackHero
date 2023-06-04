1dc3797363d294bb17cd391d3fc30fd27dbd9e43
Thiiiv <105984503+Thiiiv@users.noreply.github.com>
2023-06-04 22:01:18 +0200

    Delete BackPackHero directory

--- BackPackHero/src/fr/game/data/character/Character.java
+++
@@ -1,9 +0,0 @@
-package fr.game.data.character;
-
-public interface Character {
-	// public boolean beAttacked();
-	// public int preventAttack();
-	public void getDamage(int damage);
-	public boolean beAttacked(Character character);
-	public String getCharacterImage();
-}
